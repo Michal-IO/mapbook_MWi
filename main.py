4a3bec7cc633a529f826281de3460487cc45fcd1
Michal-IO <u56169@student.wat.edu.pl>
2025-10-29 09:10:30 +0100

    wip: added for loop with users

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello World')
+zmienna_1:str= 'Michał'
+zmienna_2:str= 'Kasia'
+zmienna_3:str= 'Albert'
+zmienna_4:str= 'Julia'
+
+
+uzytkownicy:list=[zmienna_1, zmienna_2, zmienna_3, zmienna_4]
+
+for uzytkownik in uzytkownicy:
+    print(f'Hello {uzytkownik}')
