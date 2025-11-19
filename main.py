2c11d70f3e02be063caa74a11ae985391620f9fe
Michal-IO <u56169@student.wat.edu.pl>
2025-11-19 11:49:43 +0100

    fix: frames columnspan fix

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ ramka_mapa = Frame(root)
 
 ramka_lista_obiekow.grid(row=0, column=0)
 ramka_formularz.grid(row=0, column=1)
-ramka_szczegoly_obiektu.grid(row=1, column=0)
-ramka_mapa.grid(row=2, column=0)
+ramka_szczegoly_obiektu.grid(row=1, column=0, columnspan=2)
+ramka_mapa.grid(row=2, column=0, columnspan=2)
 
 # RAMKA LISTA OBIEKTOW
 
@@ -25,13 +25,13 @@ label_lista_obiektow.grid(row=0, column=0, columnspan=3)
 listbox_lista_obiektow = Listbox(ramka_lista_obiekow)
 listbox_lista_obiektow.grid(row=1, column=0, columnspan=3)
 
-button_pokaz_szczegoly = Button(ramka_szczegoly_obiektu, text='Pokaż szczegóły')
+button_pokaz_szczegoly = Button(ramka_lista_obiekow, text='Pokaż szczegóły')
 button_pokaz_szczegoly.grid(row=2, column=0)
 
-button_usun_obiekt = Button(ramka_szczegoly_obiektu, text='Usuń obiekt')
+button_usun_obiekt = Button(ramka_lista_obiekow, text='Usuń obiekt')
 button_usun_obiekt.grid(row=2, column=1)
 
-button_edytuj_obiekt = Button(ramka_szczegoly_obiektu, text='Edytuj obiekt')
+button_edytuj_obiekt = Button(ramka_lista_obiekow, text='Edytuj obiekt')
 button_edytuj_obiekt.grid(row=2, column=2)
 
 # RAMKA FORMULARZ
@@ -64,7 +64,7 @@ entry_img_url = Entry(ramka_formularz)
 entry_img_url.grid(row=4, column=1)
 
 button_dodaj_obiekt = Button(ramka_formularz, text='Dodaj obiekt')
-button_dodaj_obiekt.grid(row=5, column=1, columnspan=2)
+button_dodaj_obiekt.grid(row=5, column=0, columnspan=2)
 
 # RAMKA SZCZEGOLY OBIEKTU
 
@@ -72,7 +72,7 @@ label_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text='Szczegoly obiektu
 label_szczegoly_obiektu.grid(row=0, column=0, sticky=W)
 
 label_imie_szczegoly_obiektu = Label(ramka_szczegoly_obiektu, text='Imie: ')
-label_szczegoly_obiektu.grid(row=1, column=0)
+label_imie_szczegoly_obiektu.grid(row=1, column=0)
 
 label_imie_szczegoly_obiektu_wartosc = Label(ramka_szczegoly_obiektu, text='....')
 label_imie_szczegoly_obiektu_wartosc.grid(row=1, column=1)
@@ -93,6 +93,6 @@ label_posty_szczegoly_obiektu_wartosc.grid(row=1, column=5)
 map_widget = tkintermapview.TkinterMapView(ramka_mapa, width=1025, height=600, corner_radius=0)
 map_widget.set_position(52.2,21.3)
 map_widget.set_zoom(6)
-map_widget.grid(row=0, column=0, columnspan=2)
+map_widget.grid(row=0, column=0)
 
 root.mainloop()
